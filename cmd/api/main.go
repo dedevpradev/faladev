efa8785ce128a6fc51e3718598852ca77a06cdbb
Marcos Fonseca <contato@marcosfonseca.com.br>
2024-07-17 19:54:07 -0300

    Correção rotina link google

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,10 +15,13 @@ func main() {
 
 	config.SetupOAuthConfig()
 
-	_, err := models.LoadToken()
+	token, err := models.LoadToken()
 
 	if err != nil {
+		log.Fatal(err)
+	}
 
+	if token == nil {
 		log.Println("Token not found, redirecting to authentication...")
 		fmt.Println("Please visit the following link to authorize your Google account: ", config.OAuthConfig.AuthCodeURL("state-token", oauth2.AccessTypeOffline))
 	}
