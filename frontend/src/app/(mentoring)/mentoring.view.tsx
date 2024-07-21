eb7d9ab34bb37a9c66727a503e9a964eb1713c0d
saagomes_meli <isaac.gomes@mercadolivre.com>
2024-07-21 11:46:03 -0300

    feat: adding API folder to make BFF

---
+++ frontend/src/app/(mentoring)/mentoring.view.tsx
@@ -0,0 +1,3 @@
+export function MentoringView() {
+	return <div></div>
+}
---
+++ frontend/src/app/api/exempla/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+
+export function GET() {
+	return NextResponse.json({
+		test: 'test',
+	})
+}
