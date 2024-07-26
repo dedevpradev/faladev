acb45dc41446ea4c025d60e17c68052917dc2b42
Marcos Fonseca <contato@marcosfonseca.com.br>
2024-07-26 17:38:35 -0300

    Customizando migration para execução de seed

--- backend/cmd/migrate/main.go
+++ backend/cmd/migrate/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"faladev/config"
 	"faladev/internal/database"
 	"faladev/internal/models"
 	"fmt"
@@ -11,7 +12,17 @@ func main() {
 
 	fmt.Println("Starting migration...")
 
-	db := database.GetDB()
+	appConfig, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	db, err := database.InitDB(appConfig.DatabaseURL)
+
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
 
 	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
 
@@ -24,8 +35,18 @@ func main() {
 	errToken := db.AutoMigrate(&models.Token{})
 
 	if errToken != nil {
-		log.Fatalf("Failed to migrate students: %v", errToken)
+		log.Fatalf("Failed to migrate tokens: %v", errToken)
+	}
+
+	errEvent := db.AutoMigrate(&models.Event{})
+
+	if errEvent != nil {
+		log.Fatalf("Failed to migrate events: %v", errEvent)
 	}
 
 	fmt.Println("Migration completed successfully!")
+
+	database.Seed(db)
+
+	fmt.Println("Seed completed successfully!")
 }
