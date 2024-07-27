aad52a5f54363b524b5528baecc2540dd23fdcc4
Marcos Fonseca <contato@marcosfonseca.com.br>
2024-07-27 20:15:15 -0300

    Implementando ValidateOrRefreshToken em token service

--- backend/internal/services/token_service.go
+++ backend/internal/services/token_service.go
@@ -1,7 +1,10 @@
 package services
 
 import (
+	"context"
+	"faladev/internal/auth"
 	"faladev/internal/repository"
+	"fmt"
 
 	"golang.org/x/oauth2"
 )
@@ -16,10 +19,34 @@ func NewTokenService(repo *repository.TokenRepository) *TokenService {
 	}
 }
 
-func (s *TokenService) CreateToken(token *oauth2.Token) error {
-	return s.repo.CreateToken(token)
+func (ts *TokenService) CreateToken(token *oauth2.Token) error {
+	return ts.repo.CreateToken(token)
 }
 
-func (s *TokenService) GetToken() (*oauth2.Token, error) {
-	return s.repo.GetToken()
+func (ts *TokenService) GetToken() (*oauth2.Token, error) {
+	return ts.repo.GetToken()
+}
+
+func (ts *TokenService) ValidateOrRefreshToken(ctx context.Context, config *oauth2.Config) (*oauth2.Token, error) {
+
+	token, err := ts.GetToken()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to load token: %w", err)
+	}
+
+	if !token.Valid() {
+
+		token, err = auth.RefreshToken(ctx, config, token)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to refresh token: %w", err)
+		}
+
+		if err := ts.CreateToken(token); err != nil {
+			return nil, fmt.Errorf("failed to save token: %w", err)
+		}
+	}
+
+	return token, nil
 }
