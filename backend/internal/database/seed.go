bb72bc104f40f9659f105c9e7eee25ea3277f861
Marcos Fonseca <contato@marcosfonseca.com.br>
2024-07-27 21:26:01 -0300

    Implementando Key e CalendarEventLink

--- backend/internal/database/seed.go
+++ backend/internal/database/seed.go
@@ -23,16 +23,18 @@ func seedEvents(db *gorm.DB) {
 		log.Println("Inserting default event...")
 
 		defaultEvent := models.Event{
-			Name:        "Mentoria (Carreira e Tecnologia)",
-			Description: "",
-			Location:    "https://meet.google.com/eam-bqde-mgd",
-			StartDate:   time.Now().Add(24 * time.Hour),
-			EndDate:     time.Now().Add(26 * time.Hour),
-			StartTime:   time.Now().Add(24 * time.Hour),
-			EndTime:     time.Now().Add(26 * time.Hour),
-			Organizer:   "",
-			Email:       "",
-			Phone:       "",
+			Name:              "Mentoria (Carreira e Tecnologia)",
+			Key:               "04mti3liihmd9u2agf8hg7kf6u",
+			Description:       "Essa é uma mentoria gratuita para quem está entrando na área de tecnologia, migrando de área ou buscando crescimento profissional.",
+			Location:          "https://meet.google.com/eam-bqde-mgd",
+			StartDate:         time.Now().Add(24 * time.Hour),
+			EndDate:           time.Now().Add(26 * time.Hour),
+			StartTime:         time.Now().Add(24 * time.Hour),
+			EndTime:           time.Now().Add(26 * time.Hour),
+			Organizer:         "Marcos Fonseca",
+			Email:             "mentoria@faladev.tech",
+			Phone:             "",
+			CalendarEventLink: "https://www.google.com/calendar/event?eid=MDRtdGkzbGlpaG1kOXUyYWdmOGhnN2tmNnVfMjAyNDA2MTJUMjIwMDAwWiBjb250YXRvQG1hcmNvc2ZvbnNlY2EuY29tLmJy",
 		}
 
 		if err := db.Create(&defaultEvent).Error; err != nil {
