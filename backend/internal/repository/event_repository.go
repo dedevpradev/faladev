a759e74b0f479e1d2109709562844f89d962ec2d
Marcos Fonseca <contato@marcosfonseca.com.br>
2024-07-28 00:52:14 -0300

    Implementação de CountEvents

--- backend/internal/repository/event_repository.go
+++ backend/internal/repository/event_repository.go
@@ -16,39 +16,39 @@ func NewEventRepository(db *gorm.DB) *EventRepository {
 	}
 }
 
-func (r *EventRepository) CreateEvent(event *models.Event) error {
-	return r.db.Create(event).Error
+func (eventRepository *EventRepository) CreateEvent(event *models.Event) error {
+	return eventRepository.db.Create(event).Error
 }
 
-func (r *EventRepository) GetEventByID(id uint) (*models.Event, error) {
+func (eventRepository *EventRepository) GetEventByID(id uint) (*models.Event, error) {
 	var event models.Event
-	err := r.db.First(&event, id).Error
+	err := eventRepository.db.First(&event, id).Error
 	if err != nil {
 		return nil, err
 	}
 	return &event, nil
 }
 
-func (r *EventRepository) UpdateEvent(event *models.Event) error {
-	return r.db.Save(event).Error
+func (eventRepository *EventRepository) UpdateEvent(event *models.Event) error {
+	return eventRepository.db.Save(event).Error
 }
 
-func (r *EventRepository) DeleteEvent(id uint) error {
-	return r.db.Delete(&models.Event{}, id).Error
+func (eventRepository *EventRepository) DeleteEvent(id uint) error {
+	return eventRepository.db.Delete(&models.Event{}, id).Error
 }
 
-func (r *EventRepository) ListEvents() ([]models.Event, error) {
+func (eventRepository *EventRepository) ListEvents() ([]models.Event, error) {
 	var events []models.Event
-	err := r.db.Find(&events).Error
+	err := eventRepository.db.Find(&events).Error
 	if err != nil {
 		return nil, err
 	}
 	return events, nil
 }
 
-func (r *EventRepository) GetNextEvent() (*models.Event, error) {
+func (eventRepository *EventRepository) GetNextEvent() (*models.Event, error) {
 	var event models.Event
-	err := r.db.
+	err := eventRepository.db.
 		//Where("start_date >= ?", time.Now()).
 		Order("start_date, start_time").
 		Debug().
@@ -57,4 +57,13 @@ func (r *EventRepository) GetNextEvent() (*models.Event, error) {
 		return nil, err
 	}
 	return &event, nil
+}
+
+func (eventRepository *EventRepository) CountEvents() (int64, error) {
+	var count int64
+	err := eventRepository.db.Model(&models.Event{}).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
 }
