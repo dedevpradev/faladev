b5d639f2ce809fae7de4a7f00200eaa456a24819
Marcos Fonseca <contato@marcosfonseca.com.br>
2024-07-27 21:26:33 -0300

    Adicionando key e CalendarEventLink na estrutura de events

--- backend/internal/models/events.go
+++ backend/internal/models/events.go
@@ -8,14 +8,16 @@ import (
 
 type Event struct {
 	gorm.Model
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Location    string    `json:"location"`
-	StartDate   time.Time `json:"start_date"`
-	EndDate     time.Time `json:"end_date"`
-	StartTime   time.Time `json:"start_time"`
-	EndTime     time.Time `json:"end_time"`
-	Organizer   string    `json:"organizer"`
-	Email       string    `json:"email"`
-	Phone       string    `json:"phone"`
+	Name              string    `json:"name"`
+	Key               string    `json:"key"`
+	Description       string    `json:"description"`
+	Location          string    `json:"location"`
+	StartDate         time.Time `json:"start_date"`
+	EndDate           time.Time `json:"end_date"`
+	StartTime         time.Time `json:"start_time"`
+	EndTime           time.Time `json:"end_time"`
+	Organizer         string    `json:"organizer"`
+	Email             string    `json:"email"`
+	Phone             string    `json:"phone"`
+	CalendarEventLink string    `json:"calendar_event_link"`
 }
