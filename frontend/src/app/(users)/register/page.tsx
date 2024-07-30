a641d8da854b1192f4bc43ef44b1d2bda4ca353a
alekgomes <lucas.maistro@gmail.com>
2024-07-30 08:59:08 -0300

    chore: abstracts common styles to root layout

--- frontend/src/app/(users)/register/page.tsx
+++ frontend/src/app/(users)/register/page.tsx
@@ -1,11 +1,18 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
+'use client'
+import { useFormState, useFormStatus } from 'react-dom'
+
 import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { createUser, type FormState } from '@/services/users'
 
 export default function RegisterPage() {
+	const [state, formAction] = useFormState(createUser, { message: '', errors: null })
+	const { pending } = useFormStatus()
+
 	return (
-		<form action="">
+		<form action={formAction}>
 			<Card className="mx-auto max-w-sm">
 				<CardHeader>
 					<CardTitle className="text-3xl">Cadastre-se!</CardTitle>
@@ -14,21 +21,23 @@ export default function RegisterPage() {
 				<CardContent className="space-y-4">
 					<div className="space-y-2">
 						<Label htmlFor="full-name">Nome Completo</Label>
-						<Input id="full-name" placeholder="João Gomes" required />
+						<Input id="full-name" placeholder="João Gomes" name="full-name" required minLength={3} />
 					</div>
 					<div className="space-y-2">
 						<Label htmlFor="email">Email</Label>
-						<Input id="email" type="email" placeholder="me@example.com" required />
+						<Input id="email" type="email" placeholder="me@example.com" name="email" required />
 					</div>
 					<div className="space-y-2">
 						<Label htmlFor="password">Senha</Label>
-						<Input id="password" type="password" required />
+						<Input id="password" type="password" name="password" required />
 					</div>
 					<div className="space-y-2">
 						<Label htmlFor="confirm-password">Confirme a Senha</Label>
-						<Input id="confirm-password" type="password" required />
+						<Input id="confirm-password" type="password" name="confirm-password" required />
 					</div>
-					<Button className="w-full">Cadastrar</Button>
+					<Button className="w-full" disabled={pending} type="submit">
+						Cadastrar
+					</Button>
 				</CardContent>
 			</Card>
 		</form>
