26597b347b8eff3f8dcfbea141e65a83c6c9c37d
alekgomes <lucas.maistro@gmail.com>
2024-08-07 09:08:42 -0300

    add: backend request from mentoring form

--- frontend/src/app/(mentoring)/mentoring.model.ts
+++ frontend/src/app/(mentoring)/mentoring.model.ts
@@ -1,9 +1,33 @@
-import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 
-import { IMentoringAgendaService } from '@/services/MentoringAgenda/IMentoringAgendaService.model';
+import { IMentoringAgendaService } from '@/services/MentoringAgenda/IMentoringAgendaService.model'
 
+const schema = z.object({
+	name: z.string(),
+	email: z.string(),
+	phone: z.string(),
+})
+
+type Schema = z.infer<typeof schema>
 
 export function useMentoringModel(service: IMentoringAgendaService) {
-	const {} = useForm()
-	return {}
+	const { register, handleSubmit } = useForm<Schema>({
+		resolver: zodResolver(schema),
+	})
+
+	const createMentoringAgenda = (data: Schema): void => {
+		const formData = new FormData()
+
+		for (const [key, value] of Object.entries(data)) {
+			formData.set(key, value)
+		}
+
+		service.SignUpMentoring(formData)
+	}
+
+	const handleOnSubmit = handleSubmit(data => createMentoringAgenda(data))
+
+	return { register, handleOnSubmit }
 }
